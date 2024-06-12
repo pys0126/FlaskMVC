c7c2eb54dddb279dabfbf47777ec5aaf99042d14
uodrad <2493919891@qq.com>
2024-06-12 12:08:08 +0800

    将WSGI服务器更改为ASGI，性能提升显著

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import platform
 from application.logic import IndexLogic
 from application.config.ServerConfig import ServerConfig
 from asgiref.wsgi import WsgiToAsgi
@@ -12,8 +11,6 @@ asgi_app: WsgiToAsgi = WsgiToAsgi(app)
 # 定义启动命令
 command: str = (f"hypercorn main:asgi_app --log-level debug -b {ServerConfig.host}:{ServerConfig.port} -w "
                 f"{ServerConfig.workers}")
-# 获取系统类型
-system: str = platform.system()
 
 if __name__ == "__main__":
     with app.app_context():
